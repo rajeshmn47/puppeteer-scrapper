797d577bf51fbf611534c44cbc1ce2cc30660cf8
rajeshmn47 <rajeshmn47@gmail.com>
2022-06-23 22:58:59 +0530

    second commit

--- pupeteer-scraper/index.js
+++ pupeteer-scraper/index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://photoclickz.com/anushka-shetty-don-hot-pics');
-  await page.screenshot({path: 'quora.png'});
+  await page.goto('https://github.com/rajeshmn47');
+  await page.screenshot({path:'quora.png'});
   await browser.close();
 })();
