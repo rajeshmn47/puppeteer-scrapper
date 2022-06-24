2a67ba43e3d5106684edc4d09204acb50273cd85
rajeshmn47 <rajeshmn47@gmail.com>
2022-06-25 03:09:25 +0530

    second commit

--- pupeteer-scraper/question.js
+++ pupeteer-scraper/question.js
@@ -1,9 +1,49 @@
-function flattenMultiArray(arr) {
-    const flattened = [].concat(...arr);
-    return flattened.some((item) => Array.isArray(item))
-      ? flattenMultiArray(flattened)
-      : flattened;
-  }
-  const multiDimensionalArr = [11, [22, 33], [44, [55, 66, [77, [88]], 99]]];
-  const flatArr = flattenMultiArray(multiDimensionalArr);
-  console.log(typeof flatArr,flatArr)
+console.log(["a"] + ["b"]); // "ab"
+console.log([2,3,4,] + [4,2,2,2]); // ""
+console.log(![] + []); // "false", because ![] returns false.
+k={}
+a='rajesh went to delhi'
+for(i=0;i<a.length;i++){
+    if(k[a[i]]){
+        k[a[i]]=k[a[i]]+1 
+    }
+    else{
+        k[a[i]]=1
+    }
+}
+function sortObject(obj) {
+    var arr = [];
+    for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+            arr.push({
+                'key': prop,
+                'value': obj[prop]
+            });
+        }
+    }
+    arr.sort(function(a, b) { return a.value - b.value; });
+    l={}
+    //arr.sort(function(a, b) { a.value.toLowerCase().localeCompare(b.value.toLowerCase()); }); //use this to sort as strings
+    for(i=0;i<arr.length;i++){
+l[arr[i].key]=arr[i].value
+    }
+    
+    
+    return l; // returns array
+}
+s=sortObject(k)
+console.log(s,k)
+const maxSpeed = {
+    car: 300, 
+    bike: 60, 
+    motorbike: 200, 
+    airplane: 1000,
+    helicopter: 400, 
+    rocket: 8 * 60 * 60
+};
+
+const sortable = Object.fromEntries(
+    Object.entries(maxSpeed).sort(([,a],[,b]) => a-b)
+);
+
+console.log(sortable);
